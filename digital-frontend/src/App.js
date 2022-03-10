15e32da615a602985ec9f371f156752e839e6237
Mittal9269 <190030026@iitsh.ac.in>
2022-03-10 12:38:45 +0530

    adding frontend files updated

--- digital-frontend/src/App.js
+++ digital-frontend/src/App.js
@@ -1,11 +1,23 @@
+import {Switch , Route , Redirect,BrowserRouter} from "react-router-dom";
 import './App.css';
+import "../node_modules/bootstrap-css-only/css/bootstrap.min.css";
+import Home from "./components/Home/Home";
+
+
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={FormPage} />
+        <Route exact path="/user" component={User} />
+        <Route component={Error} />
+      </Switch>
+    </BrowserRouter>
   );
 }
+
+
 
 export default App;
---
+++ digital-frontend/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "react";
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <H1>This is footer. </H1>
+        </div>
+    );
+}
+
+export default Footer;
